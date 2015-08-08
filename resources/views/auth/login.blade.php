eec7f8bd9437398441e6ee8b5ed92db282d321a1
Adrian Enriquez <eydreeyawn@gmail.com>
2015-08-08 22:11:10 +0800

    auth ui updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,40 @@
 @extends('layouts.default')
 
+@section('content')
 <div class="container">            
-    <div class="row">
-        <div class="col-sm-4 col-sm-offset-4">
-            <header class="text-center">
-                <h1>Log-in</h1>
-            </header>
-            <form>
-                <div class="form-group">
-                    <label>Email</label>
-                    <input name="email" type="email" class="form-control" placeholder="Your email">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input name="password" type="password" class="form-control" placeholder="Your password">
-                </div>
-                <button class="btn btn-primary" type="submit">Login</button>
-            </form>
-
-            <hr class="hr-or">
-
-            <div class="row">
-                <div class="col-sm-6">
-                    <button class="btn btn-block btn-fb">
-                        <span class="fa fa-fw fa-facebook"></span> Login via Facebook
-                    </button>
-                </div>
-                <div class="col-sm-6">
-                    <button class="btn btn-block btn-fb">
-                        <span class="fa fa-fw fa-twitter"></span> Login via Twitter
-                    </button>
-                </div>
+    <div class="auth-container">
+        <header class="text-center">
+            <h1>Log-in</h1>
+        </header>
+        <form>
+            <div class="form-group">
+                <label>Email</label>
+                <input name="email" type="email" class="form-control" placeholder="Your email">
             </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input name="password" type="password" class="form-control" placeholder="Your password">
+            </div>
+            <button class="btn btn-primary" type="submit">
+                <span class="fa fa-sign-in fa-fw"></span>
+                 Login
+            </button>
+        </form>
 
+        <hr class="hr-or">
+
+        <div class="row">
+            <div class="col-sm-6 form-group">
+                <button class="btn btn-block btn-fb">
+                    <span class="fa fa-fw fa-facebook"></span> Login via Facebook
+                </button>
+            </div>
+            <div class="col-sm-6 form-group">
+                <button class="btn btn-block btn-twitter">
+                    <span class="fa fa-fw fa-twitter"></span> Login via Twitter
+                </button>
+            </div>
         </div>
     </div>
 </div>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,38 +1,40 @@
 @extends('layouts.default')
 
-<div class="container">            
-    <div class="row">
-        <div class="col-sm-4 col-sm-offset-4">
-            <header class="text-center">
-                <h1>Sign-up</h1>
-            </header>
-            <form>
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="text" class="form-control" placeholder="Your email">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" class="form-control" placeholder="Your password">
-                </div>
-                <div class="form-group">
-                    <label>Confirm Password</label>
-                    <input type="password" class="form-control" placeholder="Your password">
-                </div>
-                <button class="btn btn-primary" type="submit">Sign-up</button>
-            </form>
-
-            <hr class="hr-or">
-
-            <div class="row">
-                <div class="col-sm-6">
-                    <button class="btn btn-block btn-fb">Sign-up via Facebook</button>
-                </div>
-                <div class="col-sm-6">
-                    <button class="btn btn-block btn-fb">Sign-up via Twitter</button>
-                </div>
+@section('content')
+<div class="container"> 
+    <div class="auth-container">
+        <header class="text-center">
+            <h1>Sign-up</h1>
+        </header>
+        <form>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" class="form-control" placeholder="Your email">
             </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" placeholder="Your password">
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" class="form-control" placeholder="Your password">
+            </div>
+            <button class="btn btn-primary" type="submit">
+                <span class="fa fa-sign-in fa-fw"></span>
+                Sign-up
+            </button>
+        </form>
 
+        <hr class="hr-or">
+
+        <div class="row">
+            <div class="col-sm-6 form-group">
+                <button class="btn btn-block btn-fb">Sign-up via Facebook</button>
+            </div>
+            <div class="col-sm-6 form-group">
+                <button class="btn btn-block btn-twitter">Sign-up via Twitter</button>
+            </div>
         </div>
     </div>
 </div>
+@endsection
