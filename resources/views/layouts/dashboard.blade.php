5a859a1f2d4f7cacf2f3f6123f6ba6633a820f1a
Adrian Enriquez <eydreeyawn@gmail.com>
2015-08-09 04:35:29 +0800

    google api

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,5 +11,7 @@
 
 	<script src="{{ asset('js/vendor/jquery.min.js') }}"></script>
 	<script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=API_KEY"></script>
+	<script src="{{ asset('js/dashboard.js') }}"></script>
 </body>
 </html>
