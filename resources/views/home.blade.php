a9d9ec9cd40dfea9fb1be98f2f513b28a50c7581
David Borromeo <borromeo_david1@yahoo.com>
2015-08-09 05:29:38 +0800

    [Revise] Homepage Style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,125 +4,122 @@
 
 @section('content')
 
-<section class="section-banner">
-    <div class="container section-phones">
-        <div class="row">
-            <div class="col-sm-12">
-                <h1> Lorem Ipsum </h1>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                <div class="app-market-container">
-                    <a href="#"><img src="{{ asset('img/google-play-btn.png') }}"></a>
-                    <a href="#"><img src="{{ asset('img/appstore-btn.png') }}" ></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<div class="home-container">
+  <section class="left-container">
+    <div class="container-fluid container-feeds">
+          <div class="row">
+              <div class="col-sm-12">
+                  <h4>Suggestions Nearby near <strong>Dagupan</strong></h4>
+                  <div class="show-container">
+                    <p><strong>Show me:</strong><span class="show-options">
+                     <a class="btn btn-sm btn-default">Badminton</a>
+                      <a class="btn btn-sm btn-default">Golf</a>
+                       <a class="btn btn-sm btn-default">Ice Skating</a>
+                        <a class="btn btn-sm btn-default">Bowling</a>
+                         <a class="btn btn-sm btn-default">Billiard</a>
+                          <a class="btn btn-sm btn-default">Paint Ball</a>
+                   </span></p>
+                  </div>
+                  <ul class="user-feeds">
+                    <li>
+                      <div class="col-sm-1"><span class="user-counter">1.</span></div>
 
-<section class="section-dark with-pad">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <img src="http://placehold.it/350x350" alt="Sample Image">
-            </div>
-            <div class="col-sm-8">
-                <h2> Error voluptates Consectetur </h2>
-                <p>Enim sequi, error voluptates consectetur fuga veritatis unde animi sint harum illum. Velit doloremque quos reiciendis explicabo laudantium, earum enim, sed. Necessitatibus esse laudantium at excepturi. Molestias quaerat est alias optio, enim possimus, quod numquam id labore, illum unde earum. Facilis placeat, magnam assumenda consequatur vel officia, dicta nihil exercitationem delectus eveniet nostrum unde consectetur recusandae iusto porro neque, consequuntur aspernatur nulla autem fugit, quos cumque et expedita ullam reiciendis? Ad accusamus natus exercitationem quaerat. Perferendis maxime ratione sit veritatis, vel deleniti quia sequi distinctio aspernatur. Et nihil maiores voluptatum.</p>
-
-                <p>Enim sequi, error voluptates consectetur fuga veritatis unde animi sint harum illum. Velit doloremque quos reiciendis explicabo laudantium, earum enim, sed. Necessitatibus esse laudantium at excepturi. Molestias quaerat est alias optio, enim possimus, quod numquam id labore, illum unde earum. Facilis placeat, magnam assumenda consequatur vel officia, dicta nihil exercitationem delectus eveniet nostrum unde consectetur recusandae iusto porro neque, consequuntur aspernatur nulla autem fugit, quos cumque et expedita ullam reiciendis? Ad accusamus natus exercitationem quaerat. Perferendis maxime ratione sit veritatis, vel deleniti quia sequi distinctio aspernatur. Et nihil maiores voluptatum.</p>
-            </div>
-        </div>
-    </div>
-
-</section>
-
-<section class="with-pad">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8">
-                <h2>Non, quibusdam, alias!</h2>
-                <p>Enim sequi, error voluptates consectetur fuga veritatis unde animi sint harum illum. Velit doloremque quos reiciendis explicabo laudantium, earum enim, sed. Necessitatibus esse laudantium at excepturi. Molestias quaerat est alias optio, enim possimus, quod numquam id labore, illum unde earum. Facilis placeat, magnam assumenda consequatur vel officia, dicta nihil exercitationem delectus eveniet nostrum unde consectetur recusandae iusto porro neque, consequuntur aspernatur nulla autem fugit, quos cumque et expedita ullam reiciendis? Ad accusamus natus exercitationem quaerat. Perferendis maxime ratione sit veritatis, vel deleniti quia sequi distinctio aspernatur. Et nihil maiores voluptatum.</p>
-
-                <p>Enim sequi, error voluptates consectetur fuga veritatis unde animi sint harum illum. Velit doloremque quos reiciendis explicabo laudantium, earum enim, sed. Necessitatibus esse laudantium at excepturi. Molestias quaerat est alias optio, enim possimus, quod numquam id labore, illum unde earum. Facilis placeat, magnam assumenda consequatur vel officia, dicta nihil exercitationem delectus eveniet nostrum unde consectetur recusandae iusto porro neque, consequuntur aspernatur nulla autem fugit, quos cumque et expedita ullam reiciendis? Ad accusamus natus exercitationem quaerat. Perferendis maxime ratione sit veritatis, vel deleniti quia sequi distinctio aspernatur. Et nihil maiores voluptatum.</p>
-            </div>
-            <div class="col-sm-4">
-                <img src="http://placehold.it/350x350" alt="Sample Image">
-            </div>
-        </div>
-    </div>
-
-</section>
-
-
-<section class="section-med with-pad">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <img src="http://placehold.it/350x350" alt="Sample Image">
-            </div>
-            <div class="col-sm-8">
-                <h2>Amet, laudantium aspernatur animi.</h2>
-                <p>Enim sequi, error voluptates consectetur fuga veritatis unde animi sint harum illum. Velit doloremque quos reiciendis explicabo laudantium, earum enim, sed. Necessitatibus esse laudantium at excepturi. Molestias quaerat est alias optio, enim possimus, quod numquam id labore, illum unde earum. Facilis placeat, magnam assumenda consequatur vel officia, dicta nihil exercitationem delectus eveniet nostrum unde consectetur recusandae iusto porro neque, consequuntur aspernatur nulla autem fugit, quos cumque et expedita ullam reiciendis? Ad accusamus natus exercitationem quaerat. Perferendis maxime ratione sit veritatis, vel deleniti quia sequi distinctio aspernatur. Et nihil maiores voluptatum.</p>
-
-                <p>Enim sequi, error voluptates consectetur fuga veritatis unde animi sint harum illum. Velit doloremque quos reiciendis explicabo laudantium, earum enim, sed. Necessitatibus esse laudantium at excepturi. Molestias quaerat est alias optio, enim possimus, quod numquam id labore, illum unde earum. Facilis placeat, magnam assumenda consequatur vel officia, dicta nihil exercitationem delectus eveniet nostrum unde consectetur recusandae iusto porro neque, consequuntur aspernatur nulla autem fugit, quos cumque et expedita ullam reiciendis? Ad accusamus natus exercitationem quaerat. Perferendis maxime ratione sit veritatis, vel deleniti quia sequi distinctio aspernatur. Et nihil maiores voluptatum.</p>
-            </div>
-        </div>
-    </div>
-
-</section>
-
-<section class=" with-pad">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="center-section">
-                    <div class="col-sm-10 col-sm-offset-2"><h3 class="text-center">Sign up</h3></div>
-                    <form class="form-horizontal">
-                      <div class="form-group">
-                        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                        <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                      <div class="user-content col-sm-9">
+                        <h3 class="user-name"><a href="#">Lorem Ipsum Santos</a></h3>
+                        <div class="col-sm-1"><span class="user-rating">9.7</span></div>
+                        <div class="user-details col-sm-11">
+                          <p class="user-address"><small>Eum sequi et nostrum iusto soluta unde quod necessitatibus cumque.</small></p>
+                          <p class="user-address"><small><span>Earum, impedit, doloribus</span> • <span class="user-date">January 23, 2015</span></small></p>
+                          <p class="user-status">Perspiciatis ducimus distinctio reiciendis minus! Alias velit culpa exercitationem voluptate, ipsam ut quod, sed veniam quasi! Pariatur beatae voluptatum, quasi!</p>
                         </div>
-                      </div>
-                      <div class="form-group">
-                        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                        <div class="col-sm-10">
-                          <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                  </div>
+                      <div class="user-image col-sm-2"><img src="http://lorempixel.com/200/200/" alt="Sample Image"></div>
+                      <div class="clearfix"></div>
+                      <div class="col-sm-11 col-sm-offset-1"><a href="#" class="user-save"><span class="fa fa-bookmark"></span> Save</a></div>
+                      <div class="clearfix"></div>
+
+                    </li>
+                    <li>
+                      <div class="col-sm-1"><span class="user-counter">2.</span></div>
+
+                      <div class="user-content col-sm-9">
+                      <h3><a href="#">Dolor Inet Santa Maria</a></h3>
+                      <div class="col-sm-1"><span class="user-rating">9.7</span></div>
+                        <div class="user-details col-sm-11">
+                          <p class="user-address"><small>Eum sequi et nostrum iusto soluta unde quod necessitatibus cumque.</small></p>
+                          <p class="user-address"><small><span>Earum, impedit, doloribus</span> • <span class="user-date">January 23, 2015</span></small></p>
+                          <p class="user-status">Perspiciatis ducimus distinctio reiciendis minus! Alias velit culpa exercitationem voluptate, ipsam ut quod, sed veniam quasi! Pariatur beatae voluptatum, quasi!</p>
                         </div>
+
                       </div>
-                      <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                          <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+                      <div class="user-image col-sm-2"><img src="http://lorempixel.com/200/200/" alt="Sample Image"></div>
+                      <div class="clearfix"></div>
+                      <div class="col-sm-11 col-sm-offset-1"><a href="#" class="user-save"><span class="fa fa-bookmark"></span> Save</a></div>
+                      <div class="clearfix"></div>
+                    </li>
+                    <li>
+                      <div class="col-sm-1"><span class="user-counter">3.</span></div>
+
+                      <div class="user-content col-sm-9">
+                      <h3><a href="#">Placeat Nemo Vamonos</a></h3>
+                      <div class="col-sm-1"><span class="user-rating">9.7</span></div>
+                        <div class="user-details col-sm-11">
+                          <p class="user-address"><small>Eum sequi et nostrum iusto soluta unde quod necessitatibus cumque.</small></p>
+                          <p class="user-address"><small><span>Earum, impedit, doloribus</span> • <span class="user-date">January 23, 2015</span></small></p>
+                          <p class="user-status">Perspiciatis ducimus distinctio reiciendis minus! Alias velit culpa exercitationem voluptate, ipsam ut quod, sed veniam quasi! Pariatur beatae voluptatum, quasi!</p>
                         </div>
-                      </div>
-                      <div class="text-separator col-sm-10 col-sm-offset-2">
-                          <strong>Or</strong>
-                      </div>
 
-                      <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-10">
-                            <div class="btns-social">
-                                <div class="btn-fb btn-social">
-                                 <button type="submit" class="btn btn-primary btn-block"><span class="fa-facebook fa"></span> Facebook</button>
-                                 </div>
-                                 <div class="btn-tweet btn-social">
-                                 <button type="submit" class="btn btn-primary btn-block"><span class="fa-twitter fa"></span> Twitter</button>
-                                  </div>
-                            </div>
+                      </div>
+                      <div class="user-image col-sm-2"><img src="http://lorempixel.com/200/200/" alt="Sample Image"></div>
+                      <div class="clearfix"></div>
+                      <div class="col-sm-11 col-sm-offset-1"><a href="#" class="user-save"><span class="fa fa-bookmark"></span> Save</a></div>
+                      <div class="clearfix"></div>
+                    </li>
+                    <li>
+                      <div class="col-sm-1"><span class="user-counter">4.</span></div>
 
+                      <div class="user-content col-sm-9">
+                      <h3><a href="#">Suscipit Voluptates Delos Reyes</a></h3>
+                      <div class="col-sm-1"><span class="user-rating">9.7</span></div>
+                        <div class="user-details col-sm-11">
+                          <p class="user-address"><small>Eum sequi et nostrum iusto soluta unde quod necessitatibus cumque.</small></p>
+                          <p class="user-address"><small><span>Earum, impedit, doloribus</span> • <span class="user-date">January 23, 2015</span></small></p>
+                          <p class="user-status">Perspiciatis ducimus distinctio reiciendis minus! Alias velit culpa exercitationem voluptate, ipsam ut quod, sed veniam quasi! Pariatur beatae voluptatum, quasi!</p>
                         </div>
+
                       </div>
-                    </form>
-                </div>
-            </div>
+                      <div class="user-image col-sm-2"><img src="http://lorempixel.com/200/200/" alt="Sample Image"></div>
+                      <div class="clearfix"></div>
+                      <div class="col-sm-11 col-sm-offset-1"><a href="#" class="user-save"><span class="fa fa-bookmark"></span> Save</a></div>
+                      <div class="clearfix"></div>
+                    </li>
+                    <li>
+                      <div class="col-sm-1"><span class="user-counter">5.</span></div>
 
-        </div>
-    </div>
+                      <div class="user-content col-sm-9">
+                      <h3><a href="#">Repellat Pepperoni</a></h3>
+                       <div class="col-sm-1"><span class="user-rating">9.7</span></div>
+                        <div class="user-details col-sm-11">
+                          <p class="user-address"><small>Eum sequi et nostrum iusto soluta unde quod necessitatibus cumque.</small></p>
+                          <p class="user-address"><small><span>Earum, impedit, doloribus</span> • <span class="user-date">January 23, 2015</span></small></p>
+                          <p class="user-status">Perspiciatis ducimus distinctio reiciendis minus! Alias velit culpa exercitationem voluptate, ipsam ut quod, sed veniam quasi! Pariatur beatae voluptatum, quasi!</p>
+                        </div>
 
-</section>
+                      </div>
+                      <div class="user-image col-sm-2"><img src="http://lorempixel.com/200/200/" alt="Sample Image"></div>
+                      <div class="clearfix"></div>
+                      <div class="col-sm-11 col-sm-offset-1"><a href="#" class="user-save"><span class="fa fa-bookmark"></span> Save</a></div>
+                      <div class="clearfix"></div>
+                    </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </section>
+  <section class="right-container">
+    <div class="container-fluid"></div>
+  </section>
+  <div class="clearfix"></div>
+</div>
 
 @endsection
