369367d581ed3df2a97e756cffef5ec30e859c92
Adrian Enriquez <eydreeyawn@gmail.com>
2015-08-08 20:23:05 +0800

    less files updates

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-default">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="enjoin-nav" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand text-primary" href="#">Enjoin</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="enjoin-nav">
+			<nav class="nav navbar-nav navbar-right">
+				<button type="button" class="btn btn-default navbar-btn">Login</button>
+				<button type="button" class="btn btn-primary navbar-btn">Sign Up</button>
+			</nav>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
