fad2931ea89541ab4bf76c285bd68724a24f9eb5
David Borromeo <borromeo_david1@yahoo.com>
2015-08-09 12:41:44 +0800

    [Modify] Modal

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,20 @@
+$('#messages').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var recipient = button.data('whatever') ;// Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find('.modal-title').text('Message to ' + recipient);
+  });
+
 var map;
 function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 10,
     center: {lat: 14.56, lng: 121}
   });
-  
-  $.get('http://localhost:8000/api/nearby', function(data) { 
+
+  $.get('http://localhost:8000/api/nearby', function(data) {
   	$.each(data, function(key, value){
   		console.info(value);
   		var image = 'http://namicon.nexucloud.com/' + value.name + '/36/2b3a42/ff530d/';
@@ -17,7 +26,7 @@ function initialize() {
 		  });
   	});
   });
-  
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
